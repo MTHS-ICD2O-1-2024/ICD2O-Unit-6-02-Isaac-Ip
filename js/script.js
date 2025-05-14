cd77c0e88a91b249537d2eadd90b0d8ad45a91a9
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-05-14 17:02:34 +0000

    trying to make cache test 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaac Ip All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaac Ip
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICD2O-Unit-6-02-Isaac-Ip/sw.js", {
+    scope: "/ICD2O-Unit-6-02-Isaac-Ip/",
+  })
+}
+
+/**
+ * This function saves cookies (the cache).
+ */
+function checkCookies() {
+
+  // input
+  document.cookie = "numberOfCookies";
+
+
+    document.getElementById('result').innerHTML = 'You have "numberOfCookies" cookies.'
+
+  }
+
+
+/**
+ * This function adds a cookie.
+ */
+function addCookie() {
+
+  // input
+  const cookies = "numberOfCookies"
+
+  // process
+  cookies = "numberOfCookies" + 1
+  }
+
+
